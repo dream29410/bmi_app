ac7a4a1be7dc12bf1f756daea46bae93efb6e4e9
dream29410 <dream29410@gmail.com>
2022-08-14 18:47:52 +0530

    add 1

--- lib/constant/App_constant.dart
+++ lib/constant/App_constant.dart
@@ -3,3 +3,17 @@ import 'package:hexcolor/hexcolor.dart';
 
 final Color mainHexColor = HexColor("#1C1C1C");
 final Color accentHexColor = HexColor("#FCC91C");
+
+class AppConst extends StatefulWidget {
+  const AppConst({Key? key}) : super(key: key);
+
+  @override
+  State<AppConst> createState() => _AppConstState();
+}
+
+class _AppConstState extends State<AppConst> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
